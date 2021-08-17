f7f75a8d4ac22199ca0fd1327cc77f2a21918fc3
Aman Sahu <sahuaman83@gmail.com>
2021-08-17 18:27:15 +0530

    Module 3: IndexedDB integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import React, {useState, useEffect} from 'react';
+import IndexedDb from './indexedDb';
 import { Row, Col } from 'antd';
 import { Card, Button } from 'antd';
+
+const indexedDb = new IndexedDb('SamagraX');
 
 
 function App() {
@@ -21,17 +24,76 @@ function App() {
   const [postStrt, setPostStrt] = useState();
   const [postEnd, setPostEnd] = useState();
 
+  const [savecmntStrt, setsaveCmntStrt] = useState();
+  const [savecmntEnd, setsaveCmntEnd] = useState();
+
+  const [savephtoStrt, setsavePhtoStrt] = useState();
+  const [savephtoEnd, setsavePhtoEnd] = useState();
+
+  const [savetodoStrt, setsaveTodoStrt] = useState();
+  const [savetodoEnd, setsaveTodoEnd] = useState();
+
+  const [savepostStrt, setsavePostStrt] = useState();
+  const [savepostEnd, setsavePostEnd] = useState();
+
+  let cmntdata=[], photodata=[], tododata=[], postdata=[];
+
+
   useEffect(() => {
-
-    setTimeout(()=>{
-      fetchComments();
-      fetchPhotos();
-      fetchTodos();
-      fetchPosts();
-      
-    }, 5000)
-
+    setTimeout(async ()=>{
+     
+      await fetchComments();
+      await fetchPhotos();
+      await fetchTodos();
+      await fetchPosts();
+      await runIndexDb();
+    },5000)
   },[])
+
+  const runIndexDb = async () => {
+    
+    await indexedDb.createObjectStore(['comments', 'photos', 'todos', 'posts']);
+    await savefetchComments();
+    await savefetchPhotos();
+    await savefetchTodos();
+    await savefetchPosts();
+  }
+
+  const savefetchComments = async () =>{
+    const savestrttime = new Date().getTime()
+    await indexedDb.putBulkValue('comments', cmntdata);
+    const saveendtime = new Date().getTime()
+
+    setsaveCmntStrt(savestrttime);
+    setsaveCmntEnd(saveendtime);
+  }
+
+  const savefetchPhotos = async () =>{
+    const savestrttime = new Date().getTime()
+    await indexedDb.putBulkValue('photos', photodata);
+    const saveendtime = new Date().getTime()
+
+    setsavePhtoStrt(savestrttime);
+    setsavePhtoEnd(saveendtime);
+  }
+
+  const savefetchTodos = async () =>{
+    const savestrttime = new Date().getTime()
+    await indexedDb.putBulkValue('todos', tododata);
+    const saveendtime = new Date().getTime()
+
+    setsaveTodoStrt(savestrttime);
+    setsaveTodoEnd(saveendtime);
+  }
+
+  const savefetchPosts = async () =>{
+    const savestrttime = new Date().getTime()
+    await indexedDb.putBulkValue('posts', postdata);
+    const saveendtime = new Date().getTime()
+
+    setsavePostStrt(savestrttime);
+    setsavePostEnd(saveendtime);
+  }
 
   const fetchComments = async () =>{
     const strttime = new Date().getTime()
@@ -40,38 +102,38 @@ function App() {
 
     setCmntStrt(strttime);
     setCmntEnd(endtime);
-    const data = await response.json();
-    console.log(data);
+    cmntdata = await response.json();
+    console.log(cmntdata);
   }
   const fetchPhotos = async () =>{
     const strttime = new Date().getTime()
     const response = await fetch('https://jsonplaceholder.typicode.com/photos');
     const endtime = new Date().getTime()
 
-    const data = await response.json();
+    photodata = await response.json();
     setPhtoStrt(strttime);
     setPhtoEnd(endtime);
-    console.log(data);
+    console.log(photodata);
   }
   const fetchTodos = async () =>{
     const strttime = new Date().getTime()
     const response = await fetch('https://jsonplaceholder.typicode.com/todos');
     const endtime = new Date().getTime()
 
-    const data = await response.json();
+    tododata = await response.json();
     setTodoStrt(strttime);
     setTodoEnd(endtime);
-    console.log(data);
+    console.log(tododata);
   }
   const fetchPosts = async () =>{
     const strttime = new Date().getTime()
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const endtime = new Date().getTime()
 
-    const data = await response.json();
+    postdata = await response.json();
     setPostStrt(strttime);
     setPostEnd(endtime);
-    console.log(data);
+    console.log(postdata);
   }
 
   const Unixtime = () =>{
@@ -79,6 +141,23 @@ function App() {
        setCurTime(new Date().getTime())
     },1000)
     return curTime;
+  }
+
+  const button1 = () =>{
+    fetchComments();
+    savefetchComments();
+  }
+  const button2 = () =>{
+    fetchPhotos();
+    savefetchPhotos();
+  }
+  const button3 = () =>{
+    fetchTodos();
+    savefetchTodos();
+  }
+  const button4 = () =>{
+    fetchPosts();
+    savefetchPosts();
   }
 
   return (
@@ -93,16 +172,16 @@ function App() {
                   <Card style={{ height: 180}}>
                     <p style={{color: 'green'}}>Start: {cmntStrt} </p>
                     <p style={{color: 'red'}}>End: {cmntEnd}</p>
-                    <p style={{color: 'green'}}>Start Save:</p>
-                    <p style={{color: 'red'}}>End Save:</p>
+                    <p style={{color: 'green'}}>Start Save: {savecmntStrt}</p>
+                    <p style={{color: 'red'}}>End Save: {savecmntEnd}</p>
                   </Card>
                 </Col>
                 <Col span={12}>
                   <Card style={{ height: 180}}>
                     <p style={{color: 'green'}}>Start: {phtoStrt}</p>
                     <p style={{color: 'red'}}>End: {phtoEnd}</p>
-                    <p style={{color: 'green'}}>Start Save:</p>
-                    <p style={{color: 'red'}}>End Save:</p>
+                    <p style={{color: 'green'}}>Start Save: {savephtoStrt}</p>
+                    <p style={{color: 'red'}}>End Save: {savephtoEnd}</p>
                   </Card>
                 </Col>
               </Row>
@@ -112,16 +191,16 @@ function App() {
                   <Card style={{ height: 180}}>
                   <p style={{color: 'green'}}>Start: {todoStrt}</p>
                     <p style={{color: 'red'}}>End: {todoEnd}</p>
-                    <p style={{color: 'green'}}>Start Save:</p>
-                    <p style={{color: 'red'}}>End Save:</p>
+                    <p style={{color: 'green'}}>Start Save: {savetodoStrt}</p>
+                    <p style={{color: 'red'}}>End Save: {savetodoEnd}</p>
                   </Card>
                 </Col>
                 <Col span={12}>
                   <Card style={{ height: 180}}>
                     <p style={{color: 'green'}}>Start: {postStrt}</p>
                     <p style={{color: 'red'}}>End: {postEnd}</p>
-                    <p style={{color: 'green'}}>Start Save:</p>
-                    <p style={{color: 'red'}}>End Save:</p>
+                    <p style={{color: 'green'}}>Start Save: {savepostStrt}</p>
+                    <p style={{color: 'red'}}>End Save: {savepostEnd}</p>
                   </Card>
                 </Col>
               </Row>
@@ -130,10 +209,10 @@ function App() {
 
               <Row>
                 <Col span={12}>
-                <Button type="primary" onClick={fetchComments}>Button 1</Button>
+                <Button type="primary" onClick={button1}>Button 1</Button>
                 </Col>
                 <Col span={12}>
-                <Button type="primary"  onClick={fetchPhotos}>Button 2</Button>
+                <Button type="primary"  onClick={button2}>Button 2</Button>
                 </Col>
               </Row>
               
@@ -141,10 +220,10 @@ function App() {
 
               <Row>
                 <Col span={12}>
-                <Button type="primary"  onClick={fetchTodos}>Button 3</Button>
+                <Button type="primary"  onClick={button3}>Button 3</Button>
                 </Col>
                 <Col span={12}>
-                <Button type="primary"  onClick={fetchPosts}>Button 4</Button>
+                <Button type="primary"  onClick={button4}>Button 4</Button>
                 </Col>
               </Row>
 
---
+++ src/indexedDb.ts
@@ -0,0 +1,39 @@
+import { IDBPDatabase, openDB } from 'idb';
+
+class IndexedDb {
+    private database: string;
+    private db: any;
+
+    constructor(database: string) {
+        this.database = database;
+    }
+
+    public async createObjectStore(tableNames: string[]) {
+        try {
+            this.db = await openDB(this.database, 1, {
+                upgrade(db: IDBPDatabase) {
+                    for (const tableName of tableNames) {
+                        if (db.objectStoreNames.contains(tableName)) {
+                            continue;
+                        }
+                        db.createObjectStore(tableName, { autoIncrement: true, keyPath: 'id' });
+                    }
+                },
+            });
+        } catch (error) {
+            return false;
+        }
+    }
+    
+    public async putBulkValue(tableName: string, values: object[]) {
+        const tx = this.db.transaction(tableName, 'readwrite');
+        const store = tx.objectStore(tableName);
+        for (const value of values) {
+            const result = await store.put(value);
+            console.log('Put Bulk Data ', JSON.stringify(result));
+        }
+    }
+
+}
+
+export default IndexedDb;
